77ef27e05981ac69f1b8c6ef17a42d8c3bfdf14e
Achilleas Achilleos <achilleasachilleos0@gmail.com>
2025-12-05 15:34:12 +0000

    feat: remove const from Text widgets in CartScreen for dynamic rendering

--- lib/views/cart_screen.dart
+++ lib/views/cart_screen.dart
@@ -119,7 +119,7 @@ class _CartScreenState extends State<CartScreen> {
             child: Image.asset('assets/images/logo.png'),
           ),
         ),
-        title: const Text(
+        title: Text(
           'Cart View',
           style: heading1,
         ),
@@ -150,7 +150,7 @@ class _CartScreenState extends State<CartScreen> {
                 children: [
                   const SizedBox(height: 20),
                   if (cart.items.isEmpty)
-                    const Text(
+                    Text(
                       'Your cart is empty.',
                       style: heading2,
                       textAlign: TextAlign.center,
