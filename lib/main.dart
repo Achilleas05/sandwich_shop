026fca43e91f11180a3483b6d3b50be0b9aaa133
Achilleas Achilleos <achilleasachilleos0@gmail.com>
2025-12-05 14:33:22 +0000

    feat: add comments to ChangeNotifierProvider setup and disable debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -13,11 +13,17 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
+      // The ChangeNotifierProvider creates a single instance of Cart and makes it
+      // available to all descendant widgets. The create function is called once,
+      // so we have a single shared cart and context is passed to it so our provider
+      // (Cart) knows where it is in the widget tree.
       create: (BuildContext context) {
         return Cart();
       },
       child: const MaterialApp(
         title: 'Sandwich Shop App',
+        // We’ve also added debugShowCheckedModeBanner: false to remove the debug
+        // banner from the app. This is a purely aesthetic change.
         debugShowCheckedModeBanner: false,
         home: OrderScreen(maxQuantity: 5),
       ),
