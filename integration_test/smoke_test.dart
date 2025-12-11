da998f3b0a4eec0b73fe84eb14a5a4380e65cb84
Achilleas Achilleos <achilleasachilleos0@gmail.com>
2025-12-11 19:09:55 +0000

    feat: add smoke test for app startup and home screen display

---
+++ integration_test/smoke_test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:sandwich_shop/main.dart' as app;
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  testWidgets('app starts and shows home screen', (tester) async {
+    app.main();
+    await tester.pumpAndSettle();
+    expect(find.text('Sandwich Counter'), findsOneWidget);
+  });
+}
